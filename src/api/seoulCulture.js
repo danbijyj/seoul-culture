412c775e22ffd715bc4c6f62da57807a160283e8
danbijyj3 <danbijyj3@gmail.com>
2026-03-08 00:43:48 +0900

    수정

--- src/api/seoulCulture.js
+++ src/api/seoulCulture.js
@@ -1,5 +1,9 @@
 export const fetchCultureData = async () => {
-    const res = await fetch(`/api/seoul`);
+    const url = import.meta.env.DEV
+        ? `/seoul/${import.meta.env.VITE_SEOUL_KEY}/json/culturalEventInfo/1/1000/`
+        : `/api/seoul`;
+
+    const res = await fetch(url);
 
     if (!res.ok) throw new Error('데이터 요청 실패');
 
